dcc0cbe783ac5ccd79c5f0a190236109cac577de
Jeremy Martin <jmar777@gmail.com>
2017-10-24 16:35:16 -0400

    add escaping for special chars

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var isFuzzyStringMatch = require('./lib/is-fuzzy-string-match');
+var buildFuzzyRegExpString = require('./lib/build-fuzzy-regexp-string');
 
 // note: this is the repetition count, so a threshld of `n` will
 // engage on the `n + 1` successive call with the same query
@@ -14,7 +15,8 @@ module.exports = function fz(candidate, query) {
   if (query === prevQuery && query.length) {
     if (++queryRepeatCount >= USE_REGEX_THRESHOLD) {
       if (queryRepeatCount === USE_REGEX_THRESHOLD) {
-        reQuery = new RegExp(query.split('').join('.*'), 'i');
+        var fuzzyRegExpString = buildFuzzyRegExpString(query);
+        reQuery = new RegExp(fuzzyRegExpString, 'i');
       }
       return reQuery.test(candidate);
     }
